1b04f81514b3cbfde696550bb0d0d15b55599206
Игнат <mrOerg@gmail.com>
2025-06-17 20:01:30 +0300

    2.0 commit

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -7,6 +7,10 @@ def main():
 files and shows a difference.')
     parser.add_argument('first_file')
     parser.add_argument('second_file')
+    parser.add_argument(
+        '-f', '--format',
+        help='set format of output'
+    )
     return parser.parse_args()
 
 
