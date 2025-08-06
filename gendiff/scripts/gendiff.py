e9d43b946918705abd38bbcbad05de3a4b922cce
Игнат <mrOerg@gmail.com>
2025-08-06 21:42:33 +0300

    6.2 commit

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -7,58 +7,131 @@ from gendiff.scripts.yaml_parser import read_yaml
 def validate_value(value):
     if isinstance(value, bool):
         return "true" if value else "false"
+    if value is None:
+        return "null"
     return value
 
 
-def generate_diff(first_file, second_file):
-    compared_data = []
-    first_file_data = first_file
-    second_file_data = second_file
-    for el2 in second_file_data.keys():
+
+def compare_files(first_file, second_file):
+    compared_data = {}
+    for el2 in second_file.keys():
         new_el = {}
-        if el2 not in first_file_data.keys():
+        if isinstance(second_file[el2], dict) and \
+        el2 in first_file.keys() and \
+        second_file[el2] != first_file[el2]:
+            new_el['status'] = 'nested'
+            new_el['new_value'] = compare_files(
+                first_file[el2], second_file[el2]
+                )
+            compared_data[el2] = new_el
+        if el2 not in first_file.keys():
             new_el['status'] = 'added'
-            new_el['new_value'] = second_file_data[el2]
-            compared_data.append({el2: new_el})
-        if el2 in first_file_data.keys() and\
-        second_file_data[el2] == first_file_data[el2]:
+            new_el['new_value'] = second_file[el2]
+            compared_data[el2] = new_el
+        if el2 in first_file.keys() and\
+        second_file[el2] == first_file[el2]:
             new_el['status'] = 'unchanged'
-            new_el['old_value'] = first_file_data[el2]
-            new_el['new_value'] = second_file_data[el2]
-            compared_data.append({el2: new_el})
-        if el2 in first_file_data.keys() and\
-        second_file_data[el2] != first_file_data[el2]:
+            new_el['old_value'] = first_file[el2]
+            new_el['new_value'] = second_file[el2]
+            compared_data[el2] = new_el
+        if not isinstance(second_file[el2], dict) and \
+        el2 in first_file.keys() and \
+        second_file[el2] != first_file[el2]:
             new_el['status'] = 'changed'
-            new_el['old_value'] = first_file_data[el2]
-            new_el['new_value'] = second_file_data[el2]
-            compared_data.append({el2: new_el})
-    for el1 in first_file_data.keys():
+            new_el['old_value'] = first_file[el2]
+            new_el['new_value'] = second_file[el2]
+            compared_data[el2] = new_el
+
+    for el1 in first_file.keys():
         new_el = {}
-        if el1 not in second_file_data.keys():
+        if el1 not in second_file.keys():
             new_el['status'] = 'removed'
-            new_el['old_value'] = first_file_data[el1]
-            compared_data.append({el1: new_el})
-    compared_data = sorted(compared_data,
-                            key=lambda x: sorted(x.keys())[0] if x else '')
+            new_el['old_value'] = first_file[el1]
+            compared_data[el1] = new_el
+
+    return compared_data
+
+
+def create_dict(dictionary, indent=0):
+    indent_str = '    ' * indent
+    result_string = '{\n'
+    for key, value in dictionary.items():
+        if isinstance(value, dict):
+            result_string += f'{indent_str}    {key}: {create_dict(value, indent + 1)}\n'
+        else:
+            result_string += f'{indent_str}    {key}: {validate_value(value)}\n'
+    result_string += f'{indent_str}}}'
+    return result_string
+
+
+def create_stylish_string(compared_data, indent=0):
+    indent_str = '    ' * indent
     result_report = '{\n'
-    for el in compared_data:
-        for sideEl in el:
-            if el[sideEl]['status'] == 'removed':
-                result_report += \
-              f'  - {sideEl}: {validate_value(el[sideEl]['old_value'])}\n'
-            if el[sideEl]['status'] == 'unchanged':
-                result_report += \
-                f'    {sideEl}: {validate_value(el[sideEl]['new_value'])}\n'
-            if el[sideEl]['status'] == 'changed':
-                result_report += \
-                f'  - {sideEl}: {validate_value(el[sideEl]['old_value'])}\n'
-                result_report += \
-                f'  + {sideEl}: {validate_value(el[sideEl]['new_value'])}\n'
-            if el[sideEl]['status'] == 'added':
-                result_report += \
-                f'  + {sideEl}: {validate_value(el[sideEl]['new_value'])}\n'
-    result_report += '}'
+    for el in sorted(compared_data.keys()):
+        status = compared_data[el]['status']
+        old_value = compared_data[el].get('old_value')
+        new_value = compared_data[el].get('new_value')
+        if status == 'nested':
+            nested_str = create_stylish_string(new_value, indent + 1)
+            result_report += f'{indent_str}    {el}: {nested_str}\n'
+        if status == 'removed':
+            if isinstance(old_value, dict):
+                dict_str = create_dict(old_value, indent + 1)
+                result_report += f'{indent_str}  - {el}: {dict_str}\n'
+            else:
+                result_report += f'{indent_str}  - {el}: {validate_value(old_value)}\n'
+        if status == 'unchanged':
+            if isinstance(new_value, dict):
+                dict_str = create_dict(new_value, indent + 1)
+                result_report += f'{indent_str}    {el}: {dict_str}\n'
+            else:
+                result_report += f'{indent_str}    {el}: {validate_value(new_value)}\n'
+        if status == 'changed':
+            if isinstance(old_value, dict):
+                dict_str = create_dict(old_value, indent + 1)
+                result_report += f'{indent_str}  - {el}: {dict_str}\n'
+            else:
+                result_report += f'{indent_str}  - {el}: {validate_value(old_value)}\n'
+            if isinstance(new_value, dict):
+                dict_str = create_dict(new_value, indent + 1)
+                result_report += f'{indent_str}  + {el}: {dict_str}\n'
+            else:
+                result_report += f'{indent_str}  + {el}: {validate_value(new_value)}\n'
+        if status == 'added':
+            if isinstance(new_value, dict):
+                dict_str = create_dict(new_value, indent + 1)
+                result_report += f'{indent_str}  + {el}: {dict_str}\n'
+            else:
+                result_report += f'{indent_str}  + {el}: {validate_value(new_value)}\n'
+    result_report += f'{indent_str}}}'
     return result_report
+
+
+def generate_diff(first_file, second_file, format_name='stylish'):
+    compared_data = compare_files(first_file, second_file)
+    if format_name == 'stylish':
+        result = create_stylish_string(compared_data)
+    return result
+    # result_report = '{\n'
+    # for el in compared_data:
+    #     for sideEl in el:
+    #         if el[sideEl]['status'] == 'removed':
+    #             result_report += \
+    #           f'  - {sideEl}: {validate_value(el[sideEl]['old_value'])}\n'
+    #         if el[sideEl]['status'] == 'unchanged':
+    #             result_report += \
+    #             f'    {sideEl}: {validate_value(el[sideEl]['new_value'])}\n'
+    #         if el[sideEl]['status'] == 'changed':
+    #             result_report += \
+    #             f'  - {sideEl}: {validate_value(el[sideEl]['old_value'])}\n'
+    #             result_report += \
+    #             f'  + {sideEl}: {validate_value(el[sideEl]['new_value'])}\n'
+    #         if el[sideEl]['status'] == 'added':
+    #             result_report += \
+    #             f'  + {sideEl}: {validate_value(el[sideEl]['new_value'])}\n'
+    # result_report += '}'
+    # return result_report
         
         
 def main():
--- gendiff/tests/test_gendiff.py
+++ gendiff/tests/test_gendiff.py
@@ -2,7 +2,7 @@ from gendiff.scripts.file_parser import read_files
 from gendiff.scripts.gendiff import generate_diff
 from gendiff.scripts.yaml_parser import read_yaml
 
-answer = '''{
+flat_answer = '''{
   - follow: false
     host: hexlet.io
   - proxy: 123.234.53.22
@@ -11,18 +11,79 @@ answer = '''{
   + verbose: true
 }'''
 
+requirseve_answer = '''{
+    common: {
+      + follow: false
+        setting1: Value
+      - setting2: 200
+      - setting3: true
+      + setting3: null
+      + setting4: blah blah
+      + setting5: {
+            key5: value5
+        }
+        setting6: {
+            doge: {
+              - wow: 
+              + wow: so much
+            }
+            key: value
+          + ops: vops
+        }
+    }
+    group1: {
+      - baz: bas
+      + baz: bars
+        foo: bar
+      - nest: {
+            key: value
+        }
+      + nest: str
+    }
+  - group2: {
+        abc: 12345
+        deep: {
+            id: 45
+        }
+    }
+  + group3: {
+        deep: {
+            id: {
+                number: 45
+            }
+        }
+        fee: 100500
+    }  
+}'''
 
-def test_gendiff():
-    first_file = 'gendiff/tests/test_data/file1.json'
-    second_file = 'gendiff/tests/test_data/file2.json'
+
+def test_flat_gendiff():
+    first_file = 'gendiff/tests/test_data/flat/file1.json'
+    second_file = 'gendiff/tests/test_data/flat/file2.json'
     data = read_files(first_file, second_file)
     compared_files = generate_diff(data[0], data[1])
-    assert compared_files == answer
+    assert compared_files == flat_answer
 
 
-def test_yaml():
-    first_file = 'gendiff/tests/test_data/file1.yml'
-    second_file = 'gendiff/tests/test_data/file2.yml'
+def test_flat_yaml():
+    first_file = 'gendiff/tests/test_data/flat/file1.yml'
+    second_file = 'gendiff/tests/test_data/flat/file2.yml'
     data = read_yaml(first_file, second_file)
     compared_files = generate_diff(data[0], data[1])
-    assert compared_files == answer
+    assert compared_files == flat_answer
+
+
+def test_requirseve_gendiff():
+    first_file = 'gendiff/tests/test_data/requirseve/file1.json'
+    second_file = 'gendiff/tests/test_data/requirseve/file2.json'
+    data = read_files(first_file, second_file)
+    compared_files = generate_diff(data[0], data[1])
+    assert compared_files == requirseve_answer
+
+
+def test_requirseve_yaml():
+    first_file = 'gendiff/tests/test_data/requirseve/file1.yml'
+    second_file = 'gendiff/tests/test_data/requirseve/file2.yml'
+    data = read_yaml(first_file, second_file)
+    compared_files = generate_diff(data[0], data[1])
+    assert compared_files == requirseve_answer
